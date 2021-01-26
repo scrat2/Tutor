e06527f52ab144d2a011f68c77139890c7fb602a
sylvain <boussignac.s@gmail.com>
2021-01-26 16:29:45 +0100

    clean code

--- lesson/views.py
+++ lesson/views.py
@@ -19,10 +19,12 @@ def connexion(request):
         if request.method == 'POST':
             # Get data from the form
             form = ConnexionForm(request.POST)
+
             # Clean data to avoid injection
             if form.is_valid():
                 username = form.cleaned_data['username']
                 password = form.cleaned_data['password']
+
                 # Check if IDs are good
                 user = authenticate(username=username, password=password)
                 if user is not None:
@@ -41,20 +43,28 @@ def connexion(request):
 
 def profil(request):
     if request.user.is_authenticated:
+
+        # Get the form from forms.py and add it to the context to send it to the template.
         form = ProfileForm()
         context = {
             'form': form
         }
+
+        # Get the current user and the corresponding profile
         user = User.objects.get(username=request.user.username)
         profile = Profiles.objects.get(user_id=user.id)
         context['nbr_heure'] = profile.nbr_heure
+
         if request.method == 'POST':
             # Get data from the form
             form = ProfileForm(request.POST)
+
             # Clean data to avoid injection
             if form.is_valid():
                 promo = form.cleaned_data['promo']
                 campus = form.cleaned_data['campus']
+
+                # Check which data need an update and save in database
                 if promo != "":
                     profile.promo = promo
                 if campus != "":
@@ -69,15 +79,20 @@ def profil(request):
 
 def home(request):
     if request.user.is_authenticated:
+
+        # Get the current user with the corresponding profile and user's groups
         user = User.objects.get(username=request.user.username)
         profile = Profiles.objects.get(user_id=user.id)
         groups = Groups.objects.filter(profileID=profile.id)
+
+        # Create list to send data
         lessons = []
         for group in groups:
             lesson = group.lessonID
             if lesson.date >= datetime.date.today():
                 lessons.append("Vous avez un cours de {}. Le {} de {} à {}".format(lesson.nom, lesson.date,
                                                                                    lesson.begin, lesson.end))
+        # Add the list in context variable
         context = {
             'messages': lessons
         }
@@ -89,12 +104,17 @@ def home(request):
 
 def add(request):
     if request.user.is_authenticated:
+
+        # Get the form from forms.py and add it to the context to send it to the template.
         lessonform = LessonForm()
         context = {
             'lessonform': lessonform
         }
         if request.method == 'POST':
+            # Get data from the form
             form = LessonForm(request.POST)
+
+            # Clean data to avoid injection
             if form.is_valid():
                 name = form.cleaned_data['name']
                 room = form.cleaned_data['room']
@@ -104,14 +124,20 @@ def add(request):
                 promo = form.cleaned_data['promo']
                 campus = form.cleaned_data['campus']
                 subject = form.cleaned_data['subject']
+
+                # Check logic of entries before to save in database
                 if date >= datetime.date.today() and begin < end:
                     lesson = Lessons(nom=name, date=date, sujet=subject, promo=promo, begin=begin, campus=campus,
                                      end=end, salle=room)
                     lesson.save()
+
+                    # Get current user and save it as teacher for the lesson
                     user = User.objects.get(username=request.user.username)
                     profile = Profiles.objects.get(user_id=user.id)
                     group = Groups(profileID=profile, lessonID=lesson, teacher=True)
                     group.save()
+
+                    # result is the message to display after the completion of the form good or error
                     context['result'] = "Ce cours a bien été enregistré"
                 else:
                     context['result'] = "Veuillez entrer des informations cohérentes"
@@ -124,29 +150,43 @@ def add(request):
 
 def search(request):
     if request.user.is_authenticated:
+
+        # Get the form from forms.py and add it to the context to send it to the template.
         searchform = SearchForm()
         context = {
             'searchform': searchform
         }
+
         if request.method == 'POST':
+            # Get data from the form
             form = SearchForm(request.POST)
+
+            # Clean data to avoid injection
             if form.is_valid():
                 name = form.cleaned_data['name']
                 date = form.cleaned_data['date']
                 promo = form.cleaned_data['promo']
                 campus = form.cleaned_data['campus']
+
+                # Get every lesson since the current day
                 finds = Lessons.objects.filter(date__gte=datetime.date.today())
+
+                # If the date entered is before the current day do the request again
                 if date is not None:
                     if date < datetime.date.today():
                         finds = Lessons.objects.filter(date=date)
                     else:
                         finds = finds.filter(date=date)
+
+                # Apply others filter if entered
                 if name != "":
                     finds = finds.filter(nom=name)
                 if promo != "":
                     finds = finds.filter(promo=promo)
                 if campus != "":
                     finds = finds.filter(campus=campus)
+
+                # List of group corresponding only the teacher line to get every informations about profile and lesson
                 groups = []
                 for lesson in finds:
                     groups.append(Groups.objects.get(lessonID=lesson, teacher=True))
@@ -160,15 +200,23 @@ def search(request):
 
 def subscribe(request, lesson_id):
     if request.user.is_authenticated:
+
+        # Get the current user and the corresponding profile
         user = User.objects.get(username=request.user.username)
         profile = Profiles.objects.get(user_id=user.id)
+
+        # Get the lesson from the id set in url and the corresponding group's entries
         lesson = Lessons.objects.get(id=lesson_id)
         group = Groups.objects.filter(lessonID=lesson)
+
+        # If more than 5 users are already registered the lesson are unavailable
         if len(group) > 5:
             context = {
                 'disponible': False
             }
         else:
+
+            # Check if the user is already registered for this lesson
             already = False
             for person in group:
                 if person.profileID == profile:
@@ -177,6 +225,7 @@ def subscribe(request, lesson_id):
                         'already': True
                     }
 
+            # Finally get success message and save the entry in the database
             if not already:
                 context = {
                     'disponible': True
